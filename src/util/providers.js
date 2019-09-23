816c80a0fe7eaac4af8902f5f0ad0827ab73234c
Owen Williams <owenoak@gmail.com>
2019-09-23 14:30:20 -0700

    Fixed build slowness by re-building with ionic app creator. CAVEAT:  hard-coding list of providers to avoid mds-providers compile requirement (for now).

--- src/util/providers.js
+++ src/util/providers.js
@@ -14,8 +14,60 @@
     limitations under the License.
  */
 
-import { providers as mdsProviders } from 'mds-core/packages/mds-providers'
+//import { providers as mdsProviders } from 'mds-core/packages/mds-providers'
 import { getConfigPaths } from 'config'
+
+// HACK HACK HACK
+// Hard-code providers for the nonce to avoid mds typescript compiling...
+const mdsProviders = [
+  {
+    "id": "2411d395-04f2-47c9-ab66-d09e9e3c3251",
+    "provider_name": "Bird",
+    "url": "https://www.bird.co",
+    "mds_api_url": "https://mds.bird.co",
+    "gbfs_api_url": "https://mds.bird.co/gbfs"
+  },
+  {
+    "id": "c20e08cf-8488-46a6-a66c-5d8fb827f7e0",
+    "provider_name": "JUMP",
+    "url": "https://jump.com",
+    "mds_api_url": "https://api.uber.com/v0.2/emobility/mds"
+  },
+  {
+    "id": "63f13c48-34ff-49d2-aca7-cf6a5b6171c3",
+    "provider_name": "Lime",
+    "url": "https://li.me",
+    "mds_api_url": "https://data.lime.bike/api/partners/v1/mds"
+  },
+  {
+    "id": "e714f168-ce56-4b41-81b7-0b6a4bd26128",
+    "provider_name": "Lyft",
+    "url": "https://www.lyft.com",
+    "mds_api_url": "https: //api.lyft.com/v1/last-mile/mds"
+  },
+  {
+    "id": "3c95765d-4da6-41c6-b61e-1954472ec6c9",
+    "provider_name": "Sherpa",
+    "mds_api_url": "https://mds.bird.co",
+    "gbfs_api_url": "https://mds.bird.co/gbfs/platform-partner/sherpa-la"
+  },
+  {
+    "id": "70aa475d-1fcd-4504-b69c-2eeb2107f7be",
+    "provider_name": "Spin",
+    "url": "https://www.spin.app",
+    "mds_api_url": "https://api.spin.pm/api/v1/mds"
+  },
+  {
+    "id": "b79f8687-526d-4ae6-80bf-89b4c44dc071",
+    "provider_name": "Wheels",
+    "url": "https://wheels.co",
+    "mds_api_url": "https://mds.getwheelsapp.com"
+  },
+  {
+    "id": "5f7114d1-4091-46ee-b492-e55875f7de00",
+    "provider_name": "Test 1"
+  }
+]
 
 // Use `getConfigPaths` to get values from current config.
 // This will throw if any paths are not defined.
